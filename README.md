1b8f70149ec0f96bb66ea114f97c73b1a05d5db2
Matt Jackson <matt@jacksonsix.com>
2023-04-23 12:35:07 -0400

    packages/configs: axum,sentry,tokio

---
+++ README.md
@@ -0,0 +1,15 @@
+# Build Log & Changes
+
+## Chapter 1
+- Toolchain: 1.69.0
+- Linker: [mold](https://github.com/rui314/mold)
+- Code Coverage: [LLVM source-based coverage](https://github.com/taiki-e/cargo-llvm-cov) w/[Codecov](https://about.codecov.io/) integration
+
+## Chapter 2
+- no change
+
+## Chapter 3
+- Framework: Axum (0.6.16)
+- Aync Runtime: Tokio (1.27.0)
+- Environment: [git-crypt](https://dev.to/heroku/how-to-manage-your-secrets-with-git-crypt-56ih), [direnv](https://direnv.net/)
+- Error Reporting: [Sentry](https://www.sentry.io)
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, fat ass world!");
+#![allow(unused)]
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    
+    // 
+    let _guard = sentry::init((
+        std::env::var("SENTRY_DSN").expect("$SENTRY_DSN must be set"),
+        sentry::ClientOptions {
+            release: sentry::release_name!(),
+            ..Default::default()
+        },
+    ));
+
+    // // region: --- Start Server
+    // let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    // println!("Listening on http://{addr}");
+    // axum::Server::bind(&addr)
+    //     .serve(routes_all.into_make_service())
+    //     .await
+    //     .unwrap();
+    // // endregion: --- Start Server
+
+    panic!("Everything is on fire!");
+    Ok(())
 }
